21f910bd405e518f1c37d04cc6f699c6b491e08b
monu <monu@trigvent.com>
2024-09-06 12:55:58 +0530

    change show the popup

--- content-script.js
+++ content-script.js
@@ -11,6 +11,50 @@ document.body.style.background="#aaccee";
         image.srcset = imagePath;
     }
 
+let btn = document.createElement("button");
+btn.innerHTML = "Click to Display all Links";
+btn.addEventListener("click",function(){
 
+});
 
+btn.className = "top_button"
+document.body.appendChild(btn);
   
+// btn.addEventListener("click",function(){
+//     console.log("yes it is clicked");
+//     let allLinkes =document.querySelectorAll("a");
+//     let linkArray = [];
+//     allLinks.forEach((item)=>{
+//         linkArray.push(item.href);
+//     })
+//     console.log(linkArray);
+// });
+
+btn.addEventListener("click", function() {
+    console.log("yes it is clicked");
+    let allLinks = document.querySelectorAll("a"); // Corrected variable name
+    let linkArray = [];
+    allLinks.forEach((item) => {
+        linkArray.push(item.href);
+    });
+    //console.log(linkArray);
+    generatePopToDisplay(linkArray);
+});
+
+function generatePopToDisplay(links){
+    let element =document.createElement("div");
+    element.id = "top_div";
+    let output = '';
+    output += `<table>
+    <tr>
+        <th>Link Href</th>
+    </tr>
+    `;
+    links.forEach((value)=>{
+        output += `<tr>
+        <td>${value}</td>
+        </tr>`;
+    });
+    element.innerHTML = output;
+    document.body.appendChild(element);
+}
